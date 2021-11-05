32289335a901876360b95c0b383fe0270e6c5529
antonin <antonin.val@gmail.com>
2021-11-05 14:44:24 +0100

    update route

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link,
 } from "react-router-dom/cjs/react-router-dom.min";
 import Sidebar from "./Components/Sidebar/Sidebar";
 import ListNotes from "./Components/ListNotes/ListNotes";
 import MainArea from "./Components/MainArea/MainArea";
 import DisplayNote from "./Components/DisplayNote/DisplayNote";
+import Settings from "./Components/Settings/Settings";
+import Weather from "./Components/Weather/Weather";
 
 function App() {
   return (
@@ -17,6 +18,8 @@ function App() {
         <Route path="/" exact component={ListNotes} />
         <Route path="/edit" exact component={MainArea} />
           <Route path="/displayNote/:id" exact component={DisplayNote} />
+          <Route path="/settings" exact component={Settings} />
+          <Route path="/weather" exact component={Weather} />
       </Switch>
     </Router>
   );
